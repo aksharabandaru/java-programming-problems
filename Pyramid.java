310ecf5aa8d1eedb902ce9c14a0d5e4fc858e043
akshara_bandaru <aksharabandaru58@gmail.com>
2025-08-12 10:20:27 +0530

    Update and rename pyramid.java to Pyramid.java
    pyramid – Star Pyramid Pattern
    This program takes an integer r as input and prints a centered pyramid pattern of stars (*) with r rows.
    Each row contains spaces followed by stars to create a symmetric pyramid shape.

--- Pyramid.java
+++ Pyramid.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class pyramid{
+class Pyramid{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int r=sc.nextInt();
@@ -13,7 +13,6 @@ class pyramid{
            }
             System.out.println();
         }
-    
 
     }
 }
