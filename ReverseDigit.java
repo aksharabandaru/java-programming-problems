7f735ac203f12baa723436837bd8c29c30162d32
akshara_bandaru <aksharabandaru58@gmail.com>
2025-08-12 10:21:56 +0530

    Update and rename reversedigit.java to ReverseDigit.java
    reversedigit – Reverse the Digits of a Number
    This program takes an integer n as input and prints the number obtained by reversing its digits.

--- ReverseDigit.java
+++ ReverseDigit.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class reversedigit{
+class ReverseDigit{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -9,6 +9,5 @@ class reversedigit{
            n=n/10;
         }
         System.out.println(ans);
-
     }
 }
