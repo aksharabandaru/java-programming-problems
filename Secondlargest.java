922a7a2eced32be45aac06af0430a3b9ece836b0
akshara_bandaru <aksharabandaru58@gmail.com>
2025-08-14 22:53:24 +0530

    Update Secondlargest.java

--- Secondlargest.java
+++ Secondlargest.java
@@ -1,51 +1,18 @@
 import java.util.*;
-class Secondlargest{
-    static int secondLargest(int[] arr){
-   int first = Integer.MIN_VALUE;
-        int second = Integer.MIN_VALUE;
 
-        for (int num : arr) {
-            if (num > first) {
-                second = first;
-                first = num;
-            } else if (num > second && num != first) {
-                second = num;
-            }
-        }
-
-        return (second == Integer.MIN_VALUE) ? -1 : second;
-
+class Solution {
+    public int getSecondLargest(int[] arr) {
+       Arrays.sort(arr);
+   int min=Integer.MIN_VALUE;
+   int max=arr[arr.length-1];
+   for (int i : arr){
+    if(i<max){
+         min=i;
     }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter size:");
-        int n=sc.nextInt();
-        System.out.println("Enter array elements: ");
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        sc.close();
-        System.out.println(secondLargest(arr));
-    }
-}
-
-
-// //import java.util.*;
-
-// class Solution {
-//     public int getSecondLargest(int[] arr) {
-//        Arrays.sort(arr);
-//    int min=Integer.MIN_VALUE;
-//    int max=arr[arr.length-1];
-//    for (int i : arr){
-//     if(i<max){
-//          min=i;
-//     }
-//    }
+   }
   
 
 
-//         return (min == Integer.MIN_VALUE) ? -1 : min; // return -1 if no second largest
-//     }
-// }
+        return (min == Integer.MIN_VALUE) ? -1 : min; // return -1 if no second largest
+    }
+}
