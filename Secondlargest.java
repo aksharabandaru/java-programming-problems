2644234fcfce05cc981b261c5890d63ae04e724b
akshara_bandaru <aksharabandaru58@gmail.com>
2025-08-14 22:52:00 +0530

    Update Secondlargest.java

--- Secondlargest.java
+++ Secondlargest.java
@@ -29,3 +29,23 @@ class Secondlargest{
         System.out.println(secondLargest(arr));
     }
 }
+
+
+// //import java.util.*;
+
+// class Solution {
+//     public int getSecondLargest(int[] arr) {
+//        Arrays.sort(arr);
+//    int min=Integer.MIN_VALUE;
+//    int max=arr[arr.length-1];
+//    for (int i : arr){
+//     if(i<max){
+//          min=i;
+//     }
+//    }
+  
+
+
+//         return (min == Integer.MIN_VALUE) ? -1 : min; // return -1 if no second largest
+//     }
+// }
