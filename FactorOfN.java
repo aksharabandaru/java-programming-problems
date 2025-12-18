8e913e24a11b801f2bfe56b2864f5ba00db8851b
akshara_bandaru <aksharabandaru58@gmail.com>
2025-12-18 17:01:40 +0530

    Create  FactorOfN.java

---
+++ FactorOfN.java
@@ -0,0 +1,14 @@
+class FactorOfN {
+    static int sum=0;
+    public static int fact(int n){
+        if(n==1){
+            return 1;
+        }
+        return n*fact(n-1);
+       
+    }
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+        
+    }
+}
