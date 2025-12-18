547b460889cb30fe643e54a08e3e1b99698d2da0
akshara_bandaru <aksharabandaru58@gmail.com>
2025-12-18 13:45:29 +0530

    Create Gcd.java

---
+++ Gcd.java
@@ -0,0 +1,15 @@
+class Gcd {
+    public static int gcd(int a,int b){
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+       
+        System.out.println(gcd(12,6));
+        
+    }
+}
